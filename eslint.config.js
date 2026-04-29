6e149660743711a93d6eb968b5ae725628bd2d58
Joaquín Díaz <diaz.joaquin.e@gmail.com>
2026-04-29 16:25:55 -0300

    Add ESLint and Prettier configuration files: create .prettierrc.json for code formatting rules and eslint.config.js for TypeScript linting setup.

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import prettierConfig from 'eslint-config-prettier'
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs['recommended'].rules,
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  prettierConfig,
+  {
+    ignores: ['node_modules/', 'dist/'],
+  },
+]
