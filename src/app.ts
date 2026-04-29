ceb003a176bbaaf93d3ef6daaaa5de418f4fdd4e
Joaquín Díaz <diaz.joaquin.e@gmail.com>
2026-04-29 17:45:26 -0300

    Integrate Swagger UI: add Swagger API documentation endpoint to app.ts for serving API documentation at '/api-docs', enhancing API usability and developer experience.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application } from 'express'
+import swaggerUi from 'swagger-ui-express'
 import { errorHandler } from './middlewares/errorHandler.js'
 import personRouter from './routes/person.routes.js'
 import activityRouter from './routes/activity.routes.js'
+import swaggerSpec from './config/swagger.js'
 
 const app: Application = express()
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.use('/persons', personRouter)
 app.use('/activities', activityRouter)
