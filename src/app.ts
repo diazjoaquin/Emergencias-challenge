4e2d5785f4f121b7fe861cfb7194d93aec2349bf
Joaquín Díaz <diaz.joaquin.e@gmail.com>
2026-04-29 16:57:31 -0300

    Integrate activity router: add activityRouter to app.ts for handling API requests under the '/activities' endpoint, enhancing the routing structure for activity management.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Application } from 'express'
 import { errorHandler } from './middlewares/errorHandler.js'
 import personRouter from './routes/person.routes.js'
+import activityRouter from './routes/activity.routes.js'
 
 const app: Application = express()
 
 app.use(express.json())
 
 app.use('/persons', personRouter)
+app.use('/activities', activityRouter)
 
 app.use(errorHandler)
 
